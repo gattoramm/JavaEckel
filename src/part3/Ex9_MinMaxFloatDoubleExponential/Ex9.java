3385703954a2ecf2686507a4887876964425c483
RAMIL ARSAEV <gattoramm@gmail.com>
2020-06-30 08:12:04 +0300

    Initial commit

---
+++ src/part3/Ex9_MinMaxFloatDoubleExponential/Ex9.java
@@ -0,0 +1,16 @@
+package part3.Ex9_MinMaxFloatDoubleExponential;
+
+public class Ex9 {
+    public static void main(String[] args) {
+        float flMax = Float.MAX_VALUE;
+        float flMin = Float.MIN_VALUE;
+        double dbMax = Double.MAX_VALUE;
+        double dbMin = Double.MIN_VALUE;
+
+        System.out.println("Max float = " + flMax);
+        System.out.println("Min float = " + flMin);
+
+        System.out.println("Max double = " + dbMax);
+        System.out.println("Min double = " + dbMin);
+    }
+}
---
+++ src/part4/Ex1_100nums/Ex1.java
@@ -0,0 +1,8 @@
+package part4.Ex1_100nums;
+
+public class Ex1 {
+    public static void main(String[] args) {
+        for(int i = 1; i<101; i++)
+            System.out.println(i);
+    }
+}
