b6ba41db6596d9e89f2d1a9c510cee2cb9608f88
RAMIL ARSAEV <gattoramm@gmail.com>
2020-10-12 22:06:11 +0300

    Добавил описание

--- src/part5/Initialization/ArrayInit/ArrayInit.java
+++ src/part5/Initialization/ArrayInit/ArrayInit.java
@@ -1,8 +1,8 @@
-package part5.Initialization.ArrayInt;
+package part5.Initialization.ArrayInit;
 
 import java.util.Arrays;
 
-public class ArrayInt {
+public class ArrayInit {
     public static void main(String[] args) {
         Integer[] a = { new Integer(1), new Integer(2), 3, }; //Autoboxing
         Integer[] b = new Integer[]{ new Integer(1), new Integer(2), 3, };
--- src/part5/Initialization/ArrayInit/ArrayNew.java
+++ src/part5/Initialization/ArrayInit/ArrayNew.java
@@ -1,4 +1,4 @@
-package part5.Initialization.ArrayInt;
+package part5.Initialization.ArrayInit;
 
 import java.util.Arrays;
 import java.util.Random;
